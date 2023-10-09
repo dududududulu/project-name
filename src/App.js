44a45ae56ec497f88e80c54f2223180817c1e56f
zieac <ian_geyan@outlook.com>
2023-10-09 17:43:15 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello EE4032
         </a>
       </header>
     </div>
